a7ca1e7ea0f660998cdcd49b9dc6b72f2c2c1c1d
CoffeeAndPy <ed.mesasan@gmail.com>
2026-03-23 13:55:17 +0100

    making fastApi template

---
+++ {{cookiecutter.project_slug}}/api/main.py
@@ -0,0 +1,11 @@
+"""
+{{cookiecutter.project_name}} API.
+
+Included endpoints in this template:
+GET /health     → liveness probe (Kubernetes / Docker Healtcheck).
+GET /info       → Proyect information.
+POST /predict   → placeholder - replace with real model
+
+"""
+
+import random
